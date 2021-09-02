c9e809bd9134d95c41aa7d67b715a9c8b0f26f96
ShahriyarAbrarHimel <himelshahriyar7@gmail.com>
2021-09-02 12:26:45 +0600

    done

---
+++ 2 Module Two - Variables, Operators, and Data Types/1 Javascript Variables/VARIABLE.js
@@ -0,0 +1,12 @@
+/*variable declaration
+var myName;
+let myName;
+
+*/
+var myname;
+myname="shahriyar abrar himel";
+
+var fName="shahriyar";
+
+console.log(myname);
+console.log(fName);
---
+++ 2 Module Two - Variables, Operators, and Data Types/1 Javascript Variables/VARIABLE_2.js
@@ -0,0 +1,29 @@
+var a;
+let b;
+a= 165147;
+b="shahriyar abrar himel";
+
+console.log(b);
+console.log(a);
+
+var a=200478;
+console.log(a);
+
+var d=25;
+var c = d;
+console.log(b);
+console.log(c);
+
+var l= a+ d;
+console.log(l);
+
+var $abrar;
+$abrar="node.js app"
+console.log($abrar);
+
+var _programingLng;
+_programingLng="php,js,python,ruby";
+console.log(_programingLng);
+
+var _UserID165147="Shahriyar Abrar Himel";
+console.log(_UserID165147);
